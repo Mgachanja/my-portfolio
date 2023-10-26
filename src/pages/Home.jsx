1522ff67271566cb23ba463362961e324b826abe
Mgachanja <markgachanja1@gmail.com>
2023-10-26 16:30:48 +0300

    homepage done and fully responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
-import background from '../assets/back.jpg'
+import home from '../assets/homecontent.jpg'
 import '../styles/home.css'
+import { DocumentScanner, LinkedIn } from '@mui/icons-material'
 function Home() {
   return (
     <div className='home'>
-    <div className='bg' style={{backgroundImage:`url(${background})`}}>
-       <h1>hello about page</h1>
-       </div>
-    </div>
+      
+          <div className='homeContent'>
+            <h1>Hello !</h1>
+            <p id='name'>My name is<span> Gachanja.</span></p>
+            <p>I'm a fullstack developer</p>
+            </div>
+            <img src={`${home}`} alt=''></img>
+              <div className='button-links'>
+                  <button>check my CV<div className='innerLogos'><DocumentScanner/></div></button>
+                  <button>linkedIn<div className='innerLogos'><LinkedIn/></div></button>
+              </div>
+          </div>
   )
 }
 
