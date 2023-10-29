1cee45f692fc3c099633e32dd211a17785b6899f
Mgachanja <markgachanja1@gmail.com>
2023-10-29 14:59:02 +0300

    contcts.css added

---
+++ src/helpers/Vara.js
@@ -0,0 +1,20 @@
+import Vara
+
+const vara = new Vara(
+    "#vara-container",
+    "https://raw.githubusercontent.com/akzhy/Vara/master/fonts/Satisfy/SatisfySL.json",
+    [
+      {
+        text: text,
+        fontSize: 40,
+        strokeWidth: 0.7,
+      },
+      {
+        text: "Some text",
+        fontSize: 26,
+      },
+      {
+        text: "Some more text"
+      }
+    ]
+  );
