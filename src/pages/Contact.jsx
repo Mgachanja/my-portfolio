adfe94e86d13aef4dd04831b4fc434a9023ba0a2
Mgachanja <markgachanja1@gmail.com>
2023-10-29 12:59:25 +0300

    contacts page complete but not responsive

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,58 @@
 import React from 'react'
+import '../styles/contacts.css'
+import { GitHub, Instagram, LinkedIn, Twitter, WavingHand, WhatsApp,} from '@mui/icons-material'
+
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className='contactsPage'>
+    <h1 id='title'>talk to me</h1>
+      <div className='sendMessage'>
+        <form className='SendEmail' method='POST'>
+          <label htmlFor='name'>full Name</label>
+          <input className='inputs' name='name' placeholder='Enter Full Name ...' type='Text'/>
+          <label htmlFor='email'>Email</label>
+          <input className='inputs' name='email' placeholder='Enter Your Email ...' type='email'></input>
+          <label htmlFor='message'>Message</label>
+          <textarea rows='6' placeholder='Enter Massage ...'
+          name='message' required>
+          </textarea>
+          <button type='submit'>Send Message</button>
+        </form>
+        </div>
+      <h1 id='title'> connect with me</h1>
+      <div className='socialsContainer'>
+          <ul>
+
+            <div className='socialContainer'>
+              <div className='icon'><LinkedIn  style={{fontSize:`${150}`, color:'skyblue'}}
+              /></div>
+              <p>ready for connections and endorsements<br/>reach out at</p>
+
+            </div><div className='socialContainer'>
+              <div className='icon'><GitHub  style={{fontSize:`${150}`, color:'white'}}/></div>
+              <p>lets collaborate on that awesome tech idea!!<br/> reachout at</p>
+
+            </div><div className='socialContainer'>
+              <div className='icon'><Twitter style={{fontSize:`${150}`, color:'blue'}}/></div>
+              <p>lets stay up-to-date and also contribute to the amazing tech trends and innovations.<br/>reach out</p>
+
+            </div><div className='socialContainer'>
+              <div className='icon'><Instagram  style={{fontSize:`${150}`,color:'hotpink'}}/></div>
+              <p>lets talk and share literally anything.<br/>reach out</p>
+            </div>
+
+            <div className='socialContainer'>
+              <div className='icon'><WhatsApp  style={{fontSize:`${150}`, color:'darkgreen'}}/></div>
+              <p>ask me about anything!!<br/>reach out</p>
+            </div>
+          </ul>
+    </div>
+        <div className='footer'>
+           <p>happy hacking !!</p> 
+           <WavingHand style={{fontSize:100 ,color:'lightyellow'}}/>
+        </div>
+    </div>
   )
 }
 
