9f39390f41a4f55603c9dc6a94cdb0ccd9321be4
Mgachanja <markgachanja1@gmail.com>
2023-10-29 18:11:58 +0300

    new home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import {useState} from 'react'
 import {Link} from 'react-router-dom'
 import '../styles/navbar.css'
 import logo from '../assets/logo.jpg'
-import { CasesOutlined, Home,  PhoneCallback, Reorder, VerifiedUserRounded } from '@mui/icons-material'
+import { CasesOutlined, DocumentScannerTwoTone, Home,  PhoneCallback, Reorder, VerifiedUserRounded } from '@mui/icons-material'
 
 function Navbar() {
   const[ToggleMobileMenu,setToggleMobileMenu] = useState(false)
@@ -10,24 +10,32 @@ function Navbar() {
   return (
   <>
    <div className='navbar'>
-    <img src={`${logo}`} alt='logo'></img>
-    <button onClick={mobileMenu}><Reorder/></button>
-    <p>Gachanja.dev</p>
-    <div className='navLinks'>
-      <Link to='/'>Home</Link>
-      <Link to='/about'>About Me</Link>
-      <Link to='/projects'>projects</Link>
-      <Link to='/contact'>Contact</Link>
+    <div className='logos'>
+      <img src={logo} alt=''></img>
     </div>
-    </div>
-    <div className={ToggleMobileMenu?'showMobileMenu':'closeMobileMenu'}>
-      <div className='mobileLinks'>
-      <Link to='/'><Home/>  Home</Link>
-      <Link to='/about'><VerifiedUserRounded/>  About Me</Link>
-      <Link to='/projects'><CasesOutlined/>  Projects</Link>
-      <Link to='/contact'><PhoneCallback/>  Contact</Link>
+    <ul className='navList'>
+      <div className='icon'>
+        <Home style={{fontSize:50 ,color:'azure'}}/>
       </div>
-    </div>
+
+      <div className='icon'>
+        <VerifiedUserRounded style={{fontSize:50 ,color:'azure'}}/>
+      </div>
+
+      <div className='icon'>
+        <CasesOutlined style={{fontSize:50 ,color:'azure'}}/>
+      </div>
+      
+       <div className='icon'>
+        <DocumentScannerTwoTone style={{fontSize:50 ,color:'azure'}}/>
+      </div>
+      
+       <div className='icon'>
+        <PhoneCallback style={{fontSize:50 ,color:'azure'}}/>
+      </div>
+
+    </ul>
+   </div>
    </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,18 +5,10 @@ import { DocumentScanner, LinkedIn } from '@mui/icons-material'
 function Home() {
   return (
     <div className='home'>
-      
           <div className='homeContent'>
-            <h1>Hello !</h1>
-            <p id='name'>My name is<span> Gachanja.</span></p>
-            <p>I'm a fullstack developer</p>
-            </div>
-            <img src={`${home}`} alt=''></img>
-              <div className='button-links'>
-                  <button>check my CV<div className='innerLogos'><DocumentScanner/></div></button>
-                  <button>linkedIn<div className='innerLogos'><LinkedIn/></div></button>
-              </div>
+            
           </div>
+    </div>
   )
 }
 
