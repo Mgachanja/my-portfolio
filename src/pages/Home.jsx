dbdf4537b3bc50f9678abb3ab20d035ac1ac554c
Mgachanja <markgachanja1@gmail.com>
2023-10-30 11:56:14 +0300

    home page complete and responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import home from '../assets/homecontent.jpg'
 import '../styles/home.css'
-import { DocumentScanner, LinkedIn } from '@mui/icons-material'
+import { Celebration, DocumentScanner, LinkedIn, PhoneEnabled, WavingHand } from '@mui/icons-material'
 function Home() {
   return (
     <div className='home'>
           <div className='homeContent'>
-            
+            <div className='home-mini-nav'>
+              <LinkedIn style={{fontSize:40,color:'skyblue',marginRight:20}}/><DocumentScanner style={{fontSize:35,color: 'white'}}/>
+            </div>
+            <div className='intro'>
+              <p className='name'>Hey, I'm<span> Gachanja .</span><WavingHand style={{fontSize:50,color:'lightyellow'}}/></p>
+              <p className='profession'><span id='me'>I'm a </span><br/><span className='full'>FULL</span><br/><span className='stack'>STACK</span><br/><span className='dev'>DEVELOPER</span></p>
+              <p className='description'>i've spent an year building and scaling things for the web<span>.</span><br/>lets connect <Celebration style={{color:'orange'}}/></p> 
+            </div>
+            <button id='btn'>contact me<PhoneEnabled/></button>
           </div>
     </div>
   )
