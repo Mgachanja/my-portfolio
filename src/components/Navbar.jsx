57a98206bdc6ab67fde8a13ff5d0e992183c0445
Mgachanja <markgachanja1@gmail.com>
2023-10-30 07:24:06 +0300

    navbar complete and responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,44 +8,74 @@ function Navbar() {
   const[ToggleMobileMenu,setToggleMobileMenu] = useState(false)
   const mobileMenu=()=>{ setToggleMobileMenu(!ToggleMobileMenu)}
   return (
-  
-   <div className='navbar'>
+  <> 
+    <div className='navbar'>
     <div className='logo'>
       <img src={logo} alt=''></img>
       <h2>Gachanja.Dev</h2>
     </div>
     <ul className='navList'>
       <div className='icon'>
-        <Home style={{fontSize:40 }}/>
+        <Link to='/' className='icons'> <Home style={{fontSize:40, color:'azure'}}/></Link>
         <Link to = '/'>Home</Link>
-
       </div>
 
       <div className='icon'>
-        <VerifiedUserRounded style={{fontSize:40 }}/>
-        <Link to = '/about' className=''>About</Link>
+      <Link to = '/about' className='icons'><VerifiedUserRounded style={{fontSize:40, color:'azure' }}/></Link>
+        <Link to = '/about'>About</Link>
       </div>
 
       <div className='icon'>
-        <CasesOutlined style={{fontSize:40 }}/>
+      <Link to = '/projects' className='icons'><CasesOutlined style={{fontSize:40, color:'azure' }}/></Link>
         <Link to = '/projects'>Projects</Link>
-
       </div>
       
        <div className='icon'>
-        <DocumentScannerTwoTone style={{fontSize:40 }}/>
+       <Link to = '/blog' className='icons'><DocumentScannerTwoTone style={{fontSize:40, color:'azure' }}/></Link>
         <Link to = '/blog'>Blog</Link>
-
       </div>
       
        <div className='icon'>
-        <PhoneCallback style={{fontSize:40 ,}}/>
+       <Link to = '/contact' className='icons'><PhoneCallback style={{fontSize:40 , color:'azure'}}/></Link>
         <Link to = '/contact'>Contact</Link>
-        
       </div>
-
     </ul>
    </div>
+
+   <div className='btn' onClick={mobileMenu}><Reorder style={{color:'azure',fontSize:40}}/></div>
+   <div className={ToggleMobileMenu?'showMobileMenu':'hideMobileMenu'}>
+   <div className='logo'>
+      <img src={logo} alt=''></img>
+      <h2>Gachanja.Dev</h2>
+    </div>
+    <ul className='navList' onClick={mobileMenu}>
+      <div className='icon'>
+        <Link to='/' className='icons'> <Home style={{fontSize:40, color:'azure'}}/></Link>
+        <Link to = '/'>Home</Link>
+      </div>
+
+      <div className='icon'>
+      <Link to = '/about' className='icons'><VerifiedUserRounded style={{fontSize:40, color:'azure' }}/></Link>
+        <Link to = '/about'>About</Link>
+      </div>
+
+      <div className='icon'>
+      <Link to = '/projects' className='icons'><CasesOutlined style={{fontSize:40, color:'azure' }}/></Link>
+        <Link to = '/projects'>Projects</Link>
+      </div>
+      
+       <div className='icon'>
+       <Link to = '/blog' className='icons'><DocumentScannerTwoTone style={{fontSize:40, color:'azure' }}/></Link>
+        <Link to = '/blog'>Blog</Link>
+      </div>
+      
+       <div className='icon'>
+       <Link to = '/contact' className='icons'><PhoneCallback style={{fontSize:40 , color:'azure'}}/></Link>
+        <Link to = '/contact'>Contact</Link>
+      </div>
+    </ul>
+   </div>
+   </>
   
   )
 }
