5c589c300ec310c72e4ddc49b3cb58d342a559b6
Mgachanja <markgachanja1@gmail.com>
2023-10-31 15:28:21 +0300

    final home page complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import home from '../assets/homecontent.jpg'
 import '../styles/home.css'
 import { Celebration, DocumentScanner, LinkedIn, PhoneEnabled, WavingHand } from '@mui/icons-material'
+import Stars from '../components/Stars'
 function Home() {
   return (
     <div className='home'>
@@ -10,6 +10,7 @@ function Home() {
               <LinkedIn style={{fontSize:40,color:'skyblue',marginRight:20}}/><DocumentScanner style={{fontSize:35,color: 'white'}}/>
               <h1>Gachanja.Dev</h1>
             </div>
+            <Stars/>
             <div className='intro'>
               <p className='name'>Hey, I'm<span> Gachanja .</span><WavingHand style={{fontSize:50,color:'lightyellow'}}/></p>
               <p className='profession'><span id='me'>I'm a </span><br/><span className='full'>FULL</span><br/><span className='stack'>STACK</span><br/><span className='dev'>DEVELOPER</span></p>
